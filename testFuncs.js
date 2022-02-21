8b6ab809b2f3e19b2bcb289c8a09c550b996592b
Garrett <garres4@vt.edu>
2022-02-21 15:11:09 -0700

    added less than or greater than tests

--- testFuncs.js
+++ testFuncs.js
@@ -1,5 +1,12 @@
 const testFuncs = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: x => x,
+    createUser: () => {
+        const user = {firstName: 'Joe'}
+        user['lastName'] = 'Nedmond';
+        return user;
+    }
 }
 
 module.exports = testFuncs
--- testFuncs.test.js
+++ testFuncs.test.js
@@ -1,9 +1,32 @@
-const testFuncs = require('./testFuncs')
+const testFuncs = require("./testFuncs");
 
-test('Add 2 + 2 to equal 4', () => {
-    expect(testFuncs.add(2, 2)).toBe(4);
+test("Add 2 + 2 to equal 4", () => {
+  expect(testFuncs.add(2, 2)).toBe(4);
 });
 
-test('Add 2 + 2 to NOT equal 5', () => {
-    expect(testFuncs.add(2, 2)).not.toBe(5);
-})
+test("Add 2 + 2 to NOT equal 5", () => {
+  expect(testFuncs.add(2, 2)).not.toBe(5);
+});
+
+test("Should be null", () => {
+  expect(testFuncs.isNull()).toBe(null);
+});
+
+test("Should be falsy", () => {
+  expect(testFuncs.checkValue(null)).toBeFalsy();
+});
+// toEqual for complex data types, toBe for primitives
+test("User should be Joe Nedmond object", () => {
+  expect(testFuncs.createUser()).toEqual({
+    firstName: "Joe",
+    lastName: "Nedmond",
+  });
+});
+
+// less than or greater than
+test("Should be under 1600", () => {
+    const load1 = 800;
+    const load2 = 800;
+
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
